db2125ded010c20fbe347cd3d667d1af24fc21c4
Owen De Waele <owendewa@student.arteveldehs.be>
2018-11-04 19:31:56 +0100

    Added notifications

--- Notification/js/main.js
+++ Notification/js/main.js
@@ -1,29 +1,39 @@
-function sendNotification(msg) {
-  let notif = new Notification(msg);
-}
+var permission = Notification.permission;
 
-function requestNotificationPermission() {
-  if (Notification && Notification.permission === 'default') {
-    Notification.requestPermission(function (permission) {
-      if (!('permission' in Notification)) {
-        Notification.permission = permission;
-      }
-    });
+function Permission (){
+  if(Notification && Notification.permission == 'default') {
+     Notification.requestPermission((permission) => {
+         if(!('permission' in Notification)) {
+             Notification.permission = permission;
+         }
+     }) 
   }
 }
-requestNotificationPermission();
+    function notify(title,text){
+      if(Notification.permission === 'granted') {
+        let notification = new Notification(title,{
+          body: text,
+        }); 
+        notification.onclick=function(){
+          window.open('https://google.com');
+        };
+        setTimeout(notification.close.blind(notification),3000);
+      }
+    };
+
+Permission();
+
 document.getElementById("signup").addEventListener('click', signup);
 document.getElementById("login").addEventListener('click', login);
 document.getElementById("publish").addEventListener('click', publish);
 
 function signup(e){
-  e.preventDefault();
   let email = document.getElementById("signup_email").value;
   let password = document.getElementById("signup_password").value;
   firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then(function(response){   
-    let thanks = "Bedankt voor uw account te maken bij ons" ;  
-     sendNotification(thanks);
+  .then(function(response){  
+    notify('U bent geregistreed!','bedankt voor het aanmaken van uw account')
+
     })
    
     .catch(function(error) {
@@ -32,15 +42,17 @@ function signup(e){
     var errorMessage = error.message;
     // show errors in console
     document.querySelector('.singup_errorCode').innerHTML = errorMessage;
-
+      
   });
 }
+
 function login(e){
   e.preventDefault();
   let email = document.getElementById("login_email").value;
   let password = document.getElementById("login_password").value;
   firebase.auth().signInWithEmailAndPassword(email, password)
-  .then(function(resp){   
+  .then(function(resp){ 
+    notify('U bent ingelogd!')
      console.log('ingelogd');
      document.getElementById('userInfo').innerHTML = email;
     })
@@ -48,7 +60,12 @@ function login(e){
       document.getElementById("singupform").style.display="none";
       document.getElementById("loginform").style.display="none";
       document.querySelector('.formBoxPost').style.display="block";
+      let buttons = document.querySelectorAll(".deleteKnop");
+      for(let i = 0; i<buttons.length; i++){
+        buttons[i].style.display="block";
+      }
     })
+      
     .catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -57,15 +74,15 @@ function login(e){
     document.querySelector('.login_errorCode').innerHTML = errorMessage;
     
   });
-
 }
 
 function publish(e){
-  e.preventDefault();  
+  e.preventDefault(); 
+   
   let email = document.getElementById('userInfo').innerHTML;
   let titel = document.getElementById("titel").value;
   let blog = CKEDITOR.instances.editor1.getData();
-  let auteur = document.getElementById("auteur").value;
+  let auteur = email;
   let date = new Date();  
   
   let articleData = {
@@ -76,18 +93,13 @@ function publish(e){
     date_edited: firebase.database.ServerValue.TIMESTAMP,
     uid: email,
   };
-  let key = firebase.database().ref(
-    'article').push().key;
+  let key = database.ref('article').push().key;
   let updates = {};
   updates['article/' + key] = articleData;
-  
-  return firebase
-    .database()
-    .ref()
-    .update(updates)
+  return database.ref().update(updates)
     .then(function(){
-        alert('Added ' + titel);
-
+      notify('Uw artikel is aangemaakt!')
+      
     })
     .catch(function(error) {
         console.log(error);
@@ -104,7 +116,6 @@ function getData(data){
     let auteur = articles[k].auteur;
     let content = articles[k].body;
     let datum = articles[k].date_added;
-
     console.log(titel,auteur,content,datum);
     let box = document.getElementById('postList');
     let createTitel = document.createElement('H1');
@@ -115,17 +126,37 @@ function getData(data){
     createAuteur.innerHTML = auteur;
     let createDatum = document.createElement('H3');
     createDatum.innerHTML = datum;
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'delete';
+    deleteButton.classList.add('deleteKnop');
     let post = document.createElement('div');
     post.appendChild(createTitel);
     post.appendChild(createContent);
     post.appendChild(createAuteur);
     post.appendChild(createDatum);
+    post.appendChild(deleteButton);
     box.appendChild(post);
-     
+    let buttons = document.querySelectorAll(".deleteKnop");
+    for(let i = 0; i<buttons.length; i++){
+      buttons[i].addEventListener('click', remove);
+    }
   }
 }
+
+function remove(){
+    ref.on('value',function(snapshot){
+      const data = snapshot.val() || null;
+      const id = Object.keys(data);
+      let buttons = document.querySelectorAll(".deleteKnop");
+      for(let i = 0; i<buttons.length; i++){
+        number = i;
+        buttons[i].addEventListener('click', function(){
+          console.log(id[number]);
+        })
+      }      
+    })
+  }
 
 function errData(err){
   console.log(err);
 }
-  
